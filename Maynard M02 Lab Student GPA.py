dc40771898e9942a3360e323c13f7d4a8c63f869
Lukas Maynard <lmaynard8@ivytech.edu>
2022-06-13 17:03:41 -0400

    Version 1

---
+++ Maynard M02 Lab Student GPA.py
@@ -0,0 +1,29 @@
+"""
+GPA Indicator for Honars/Deans list
+Lukas Maynard
+SDEV - 220
+This will show if a student is in honors or on the Deans list based on their GPA
+"""
+def studentInfo(name, gpa):
+    print('-------------------------------')
+    print('Name:', name)
+    print('GPA:', gpa)
+    if gpa >= 3.5:
+        print('Student is on the Deans list.')
+    elif 3.25 <= gpa < 3.5:
+        print('Student is on the Honor Roll.')
+    else:
+        print('Student is not on Honor Roll or Deans list.')
+    print('-------------------------------\n')
+
+print('\nFind out if a student is in Honors or on the Deans list.\
+        \nEnter "ZZZ" as the student name to stop program')
+
+while True:
+    name = str(input('Enter students first and last name: '))
+    if name == 'ZZZ':
+        print('Thanks for using this GPA indicator.\n')
+        break
+    gpa = float(input('Enter student GPA: '))
+    studentInfo(name, gpa)
+    
