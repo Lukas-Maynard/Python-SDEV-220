9f7595323024af12980723fba285a028a7002df0
Lukas Maynard <lmaynard8@ivytech.edu>
2022-06-22 12:59:08 -0400

    added assignment

---
+++ M03/Repl Tutorials/Maynard M03 Tutorial E146.py
@@ -0,0 +1,8 @@
+# One line lambda expression that prints a squares list
+mylist = [5,4,3]
+print(list(map(lambda i:i**2, mylist)))
+
+# Sort a list of tuples by the second value by declaring the key with a lambda function
+a = [(0,2), (4,3), (9,9), (10,-1)]
+a.sort(key=lambda i:i[1])
+print(a)
