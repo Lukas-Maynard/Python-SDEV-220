72ab3f1059fbc4d6db66bdd9387bffc6f1aa3be1
Lukas Maynard <lmaynard8@ivytech.edu>
2022-07-01 19:36:58 -0400

    fixed read problem

--- M04/Maynard M04 Programming Assignment/16.8 with sqlalchemy.py
+++ M04/Maynard M04 Programming Assignment/16.8 with sqlalchemy.py
@@ -1,21 +1,26 @@
+"""
+reads the given database and prints every line.
+"""
 import sqlalchemy as sa
 
 # table called books with these fields: title (text), author (text), and year (integer).
 # conn = sqlite3.connect('books.db')
-conn = sa.create_engine('sqlite:///:memory:')
 
-conn.execute("""CREATE TABLE books (
-            title text,
-            author text,
-            year integer
-            )""")
+path = 'books2.db'
+conn = sa.create_engine(f'sqlite:///{path}')
 
-ins = 'INSERT INTO books (title, author, year) VALUES (?, ?, ?)'
-conn.execute(ins, ('The Weirdstone of Brisingamen','Alan Garner',1960))
-conn.execute(ins, ('Perdido Street Station', 'China Miéville',2000))
-conn.execute(ins, ('Thud!', 'Terry Pratchett',2005))
-conn.execute(ins, ('The Spellman Files', 'Lisa Lutz',2007))
-conn.execute(ins, ('Small Gods', 'Terry Pratchett',1992))
+# conn.execute("""CREATE TABLE books (
+#             title text,
+#             author text,
+#             year integer
+#             )""")
+
+# ins = 'INSERT INTO books (title, author, year) VALUES (?, ?, ?)'
+# conn.execute(ins, ('The Weirdstone of Brisingamen','Alan Garner',1960))
+# conn.execute(ins, ('Perdido Street Station', 'China Miéville',2000))
+# conn.execute(ins, ('Thud!', 'Terry Pratchett',2005))
+# conn.execute(ins, ('The Spellman Files', 'Lisa Lutz',2007))
+# conn.execute(ins, ('Small Gods', 'Terry Pratchett',1992))
 
 
 rows = conn.execute('SELECT * FROM books')
