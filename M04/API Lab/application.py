cd7a87dddb23e23b7724ed46773b96a82d3ee94f
Lukas Maynard <lmaynard8@ivytech.edu>
2022-07-05 00:43:11 -0400

    fixed bugs

--- M04/API Lab/application.py
+++ M04/API Lab/application.py
@@ -1,6 +1,55 @@
-from flask import Flask
+"""
+"Book" CRUD API with flask
+Lukas Maynard
+SDEV - 220
+Stores, gets, adds, and deletes books
+"""
+from flask import Flask, request
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(80))
+    publisher = db.Column(db.String(80))
+
+    def __repr__(self):
+        return f'{self.name} - {self.author}'
 
 @app.route('/')
 def index():
     return 'Hello!'
+
+@app.route('/books')
+def get_books():
+    books = Book.query.all()
+    output = []
+    for book in books:
+        book_data = {'name':book.name, 'author': book.author}
+        output.append(book_data)
+    return {"books":output}
+
+@app.route('/books/<id>')
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return {'name':book.name, 'author': book.author}
+
+@app.route('/books', method=['POST'])
+def add_book():
+    book = Book(name=request.json['name'], author=request.json['author'])
+    db.session.add(book)
+    db.session.commit()
+    return {'id': book.id}
+
+@app.route('/books/<id>', method=['DELETE'])
+def delete_book(id):
+    book = Book.query.get(id)
+    if book is None:
+        return {'error': 'not found'}
+    db.session.add(book)
+    db.session.commit()
+    return {'message': 'its working'}
